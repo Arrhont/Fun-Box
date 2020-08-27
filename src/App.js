a32e325ab0ab081202c0f4ea194ac1e193885748
Селянин Евгений Михайлович <selyanin.em@elektro.local>
2020-08-27 12:29:39 +0300

    selecting works

--- src/App.js
+++ src/App.js
@@ -3,60 +3,72 @@ import './App.css';
 import ProductCard from './ProductCard';
 import { CARD_STATE } from './consts';
 
-
 const mock = [
-  {
-    id: 1,
-    marketingText: 'Сказочное заморское яство',
-    brandName: 'Нямушка',
-    flavor: 'с фуа-гра',
-    portionCount: 10,
-    mouseGiftCount: 1,
-    imageUrl: '/nice_cat.png',
-    weight: 0.5,
-    description: 'Печень утки разварная с артишоками'
-  },
-  {
-    id: 2,
-    marketingText: 'Сказочное заморское яство',
-    brandName: 'Нямушка',
-    flavor: 'с фуа-гра',
-    portionCount: 10,
-    mouseGiftCount: 1,
-    imageUrl: '/nice_cat.png',
-    weight: 0.5,
-    description: 'Печень утки разварная с артишоками'
-  },
-  {
-    id: 3,
-    marketingText: 'Сказочное заморское яство',
-    brandName: 'Нямушка',
-    flavor: 'с фуа-гра',
-    portionCount: 10,
-    mouseGiftCount: 1,
-    imageUrl: '/nice_cat.png',
-    weight: 0.5,
-    description: 'Печень утки разварная с артишоками'
-  },
-]
+    {
+        id: 1,
+        marketingText: 'Сказочное заморское яство',
+        brandName: 'Нямушка',
+        flavor: 'с фуа-гра',
+        portionCount: 10,
+        mouseGiftCount: 1,
+        imageUrl: '/nice_cat.png',
+        weight: 0.5,
+        description: 'Печень утки разварная с артишоками',
+        quantity: 1,
+    },
+    {
+        id: 2,
+        marketingText: 'Сказочное заморское яство',
+        brandName: 'Нямушка',
+        flavor: 'с фуа-гра',
+        portionCount: 10,
+        mouseGiftCount: 1,
+        imageUrl: '/nice_cat.png',
+        weight: 0.5,
+        description: 'Печень утки разварная с артишоками',
+        quantity: 2,
+    },
+    {
+        id: 3,
+        marketingText: 'Сказочное заморское яство',
+        brandName: 'Нямушка',
+        flavor: 'с фуа-гра',
+        portionCount: 10,
+        mouseGiftCount: 1,
+        imageUrl: '/nice_cat.png',
+        weight: 0.5,
+        description: 'Печень утки разварная с артишоками',
+        quantity: 0,
+    },
+];
 
 function App() {
-  const [products, setProducts] = useState(mock);
-  return (
-    <div className="App">
-      <div className="App-Header">
-        Ты сегодня покормил кота?
-      </div>
-      <div className="App-Products">
-        {products.map(product => <ProductCard
-          className="App-ProductItem"
-          cardState={CARD_STATE.DEFAULT}
-          product={product}
-          key={product.id}
-        />)}
-      </div>
-    </div>
-  );
+
+    const [products, setProducts] = useState(mock);
+    const [selectedProducts, setSelectedProducts] = useState(new Set());
+
+    return (
+        <div className="App">
+            <div className="App-Header">Ты сегодня покормил кота?</div>
+            <div className="App-Products">
+                {products.map((product) => (
+                    <ProductCard
+                        className="App-ProductItem"
+                        cardState={
+                            product.quantity === 0
+                                ? CARD_STATE.DISABLED
+                                : selectedProducts.has(product)
+                                ? CARD_STATE.SELECTED
+                                : CARD_STATE.DEFAULT
+                        }
+                        setSelectedProducts={setSelectedProducts}
+                        product={product}
+                        key={product.id}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,19 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import plural from 'plural-ru';
 import './ProductCard.css';
 import { CARD_STATE } from './consts';
 
 function ProductCard(props) {
-    const { cardState } = props;
+    // useEffect(() => {
+    //     console.log('mount');
+
+    //     return () => {
+    //         debugger;
+    //         console.log('unmount');
+    //     };
+    // }, []);
+
+    const { cardState, setSelectedProducts, className, product } = props;
 
     const [hovered, useHovered] = useState(false);
-    const { className, product } = props;
     return (
         <div className={className}>
             <div className="ProductCard-Border">
                 <div
-                className="ProductCard-Card"
-                style={{backgroundImage: `url(${product.imageUrl})`}}
+                    className="ProductCard-Card"
+                    style={{ backgroundImage: `url(${product.imageUrl})` }}
+                    onClick={() => {
+                        setSelectedProducts((prevState) => {
+                            if (cardState !== CARD_STATE.DISABLED) {
+                                prevState.delete(product);
+                            }
+                            return new Set([...prevState]);
+                        });
+                    }}
                 >
                     <div className="ProductCard-MarkeingText">
                         {product.marketingText}
@@ -21,17 +37,28 @@ function ProductCard(props) {
                     <div className="ProductCard-BrandName">
                         {product.brandName}
                     </div>
-                    <div className="ProductCard-Flavor">
-                        {product.flavor}
-                    </div>
+                    <div className="ProductCard-Flavor">{product.flavor}</div>
                     <div className="ProductCard-PortionCount">
-                        {`${plural(product.portionCount, '%d порция', '%d порции', '%d порций')}`}
+                        {`${plural(
+                            product.portionCount,
+                            '%d порция',
+                            '%d порции',
+                            '%d порций'
+                        )}`}
                     </div>
                     <div className="ProductCard-MouseGiftCount">
-                    {`${plural(product.mouseGiftCount, 'мышь', '%d мыши', '%d мышей')} в подарок`}
+                        {`${plural(
+                            product.mouseGiftCount,
+                            'мышь',
+                            '%d мыши',
+                            '%d мышей'
+                        )} в подарок`}
                     </div>
                     <div className="ProductCard-Weight">
-                        <div>{`${String(product.weight).replace('.', ',')}`}</div>
+                        <div>{`${String(product.weight).replace(
+                            '.',
+                            ','
+                        )}`}</div>
                         <div className="ProductCard-WeightUnit">КГ</div>
                     </div>
                 </div>
@@ -39,20 +66,26 @@ function ProductCard(props) {
             <div className="ProductCard-Tooltip">
                 {cardState === CARD_STATE.DEFAULT && (
                     <span>
-                        Чего стоишь? Порадуй коте, <a href="1"> купи</a>
+                        Чего стоишь? Порадуй коте,{' '}
+                        <span
+                            className="ProductCard-Link"
+                            onClick={() => {
+                                setSelectedProducts(
+                                    (prevState) =>
+                                        new Set([...prevState, product])
+                                );
+                            }}
+                        >
+                            купи
+                        </span>
                     </span>
                 )}
-                {cardState === CARD_STATE.SELECTED && (
-                        product.description
-                )}
-                {cardState === CARD_STATE.DISABLED && (
-                        `Печалька, ${product.flavor} закончился`
-                )}      
+                {cardState === CARD_STATE.SELECTED && product.description}
+                {cardState === CARD_STATE.DISABLED &&
+                    `Печалька, ${product.flavor} закончился`}
             </div>
         </div>
     );
 }
 
-
 export default ProductCard;
-
